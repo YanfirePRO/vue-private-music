3cde828ad33ae978726e72d83a2828acdab6c6f0
ycj <yanchengjie2011@163.com>
2017-09-18 14:36:13 +0800

    之前的代码

---
+++ src/components/singer/singer.vue
@@ -0,0 +1,101 @@
+<template>
+	<div>singer</div>
+</template>
+
+<script>
+	import {getSingerList} from 'api/singer'
+	import {ERR_OK} from 'api/config'
+	import Singer from 'common/js/singer'
+
+	const HOT_NAME = '热门'
+	const HOT_SINGER_LEN = 10
+
+	export default{
+		data(){
+			return{
+				singers: []
+			}
+		},
+		created(){
+			this._getSingerList()
+		},
+		methods:{
+			_getSingerList(){
+				getSingerList().then(res=>{
+					if(res.code === ERR_OK){
+						this.singers = res.data.list
+						console.log("res:",this.singers)
+						this.normalizeSinger(this.singers)
+					}
+				})
+			},
+			normalizeSinger(list){
+
+				let map ={
+					hot: {
+						title: HOT_NAME,
+						items: []
+					}
+				}
+
+				list.forEach((item, index) => {
+					if(index < HOT_SINGER_LEN){
+						map.hot.items.push(new Singer({
+							id: item.Fsinger_mid,
+							name: item.Fsinger_name
+						})
+						)
+					}
+
+					const key = item.Findex
+
+					if(!map[key]){
+						map[key] = {
+							title: key,
+							items: []
+						}
+					}
+
+					map[key].items.push(new Singer({
+							id: item.Fsinger_mid,
+							name: item.Fsinger_name
+						})
+					)
+				})
+				console.log("map:",map)
+
+				//对象的遍历是无序的，所以需要将对象排序
+				let hot = []
+				let ret = []
+
+				for (let key in map){
+					let val = map[key]
+					if (val.title.match(/[a-zA-z]/)){
+						ret.push(val)
+					}
+					else if (val.title === HOT_NAME){
+						hot.push(val)
+					}
+				}
+
+				//排序
+				ret.sort((a, b) => {
+					return a.title.charCodeAt(0) - b.title.charCodeAt(0);
+				})
+
+				hot = hot.concat(ret);
+				console.log("arr:", hot)
+
+				return  hot
+			}
+		}
+	}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+	.singer
+		position fixed
+		top 88px
+		bottom 0
+		width 100%
+</style>
