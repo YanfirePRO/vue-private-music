e0a8e1c05cad6547f5f5ff637ca8d493367877fc
ycj <yanchengjie2011@163.com>
2017-09-19 15:43:45 +0800

    1、scroll组件新增scroll事件 2、listview组件实现左右对应

--- src/base/listview/listview.vue
+++ src/base/listview/listview.vue
@@ -1,5 +1,5 @@
 <template>
-	<scroll ref="listview" class="listview" :data="data">
+	<scroll ref="listview" class="listview" :data="data" :listenScroll="listenScroll" @scroll="scroll" :probeType="probeType">
 		<ul>
 			<li ref="listGroup" v-for="group in data" class="list-group">
 				<h2 class="list-group-title">{{group.title}}</h2>
@@ -13,7 +13,7 @@
 		</ul>
 		<div class="list-shortcut" @touchstart="onShortcutTouchStart" @touchmove.stop.prevent="onShortcutTouchMove">
 			<ul>
-				<li v-for="(item,index) in shortcutList" class="item" :data-index="index">
+				<li v-for="(item,index) in shortcutList" class="item" :class="{'current': currentIndex === index}" :data-index="index">
 				{{item}}
 				</li>
 			</ul>
@@ -38,8 +38,17 @@
 				default: []
 			}
 		},
+		data(){
+			return {
+				scrollY: -1,
+				currentIndex: 0
+			}
+		},
 		created(){
 			this.touch = {}
+			this.listenScroll = true
+			this.listHeight = []
+			this.probeType = 3
 		},
 		computed:{
 			shortcutList(){
@@ -48,8 +57,29 @@
 				})
 			}
 		},
+		watch:{
+			data(val){
+				setTimeout(() => {
+					this._calculateHeight()
+				}, 20)
+			},
+			scrollY(newY){
+				const listHeight = this.listHeight
+				for(let i = 0; i < listHeight.length; i++){
+					let height1 = listHeight[i]
+					let height2 = listHeight[i+1]
+					if(!height2 || (-newY) > height1 && (-newY) < height2){
+						this.currentIndex = i
+						console.log("currentIndex:", this.currentIndex)
+						return
+					}
+				}
+				this.currentIndex = 0
+			}
+		},
 		methods:{
 			onShortcutTouchStart(e){
+				console.log(e)
 				let anchorIndex = getData(e.target, 'index')
 				let firstTouch = e.touches[0]
 				this.touch.y1 = firstTouch.pageY
@@ -67,6 +97,22 @@
 			},
 			_scrollTo(index){
 				this.$refs.listview.scrollToElement(this.$refs.listGroup[index], 0)
+			},
+			//监听滚动事件
+			scroll(pos){
+				this.scrollY = pos.y
+			},
+			//计算高度
+			_calculateHeight(){
+				this.listHeight = []
+				const list = this.$refs.listGroup
+				let height = 0
+				this.listHeight.push(height)
+				for(let i = 0; i < list.length; i++){
+					let item = list[i]
+					height += item.clientHeight
+					this.listHeight.push(height)
+				}
 			}
 		}
 	}
--- src/base/scroll/scroll.vue
+++ src/base/scroll/scroll.vue
@@ -24,6 +24,10 @@
 			refreshDelay: {
         		type: Number,
         		default: 20
+      		},
+      		listenScroll: {
+      			type: Boolean,
+      			default: false
       		}
 		},
 		mounted(){
@@ -41,6 +45,13 @@
 					probeType: this.probeType,
 					click: this.click
 				})
+
+				if (this.listenScroll){
+					let me = this
+					this.scroll.on("scroll", (pos) => {
+						me.$emit('scroll', pos)
+					})
+				}
 			},
 
 			enable(){
